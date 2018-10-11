1317f671eb0982dc22c239117c488831ef566ab2
Ivar Loftsson <ivar15@ru.is>
2018-10-11 23:42:46 +0000

    No input returns 0

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,6 +1,8 @@
 // StringCalculator.js
 function Add(num1, num2) {
 	var value = (+num1) + (+num2)
+	if(isNaN(value))
+		value = 0;
 	return value.toString();
 }
 module.exports = Add;
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,5 +1,12 @@
 // StringCalculator.test.js
 const StringCalculator = require('./StringCalculator');
-test("returns sum of input", () => {
+
+test("return with no input", () => {
+	expect(StringCalculator()).toBe("0");
+});
+
+test("returns sum of 2 values", () => {
 	expect(StringCalculator("1","2")).toBe("3");
 });
+
+

9c1d7f74a291c2da88597f15c520d3a3508f6c8a
Ivar Loftsson <ivar15@ru.is>
2018-10-11 23:45:58 +0000

    Input can be 1 value

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,8 +1,11 @@
 // StringCalculator.js
 function Add(num1, num2) {
-	var value = (+num1) + (+num2)
-	if(isNaN(value))
-		value = 0;
+	var value;
+	if(isNaN(num1))
+		num1 = 0;
+	if(isNaN(num2))
+		num2 = 0;
+	value = (+num1) + (+num2);
 	return value.toString();
 }
 module.exports = Add;
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -5,6 +5,10 @@ test("return with no input", () => {
 	expect(StringCalculator()).toBe("0");
 });
 
+test("returns sum of 1 value", () => {
+	expect(StringCalculator("1")).toBe("1");
+});
+
 test("returns sum of 2 values", () => {
 	expect(StringCalculator("1","2")).toBe("3");
 });
